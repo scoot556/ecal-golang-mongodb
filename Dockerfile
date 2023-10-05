286dbe40bae75d47bf9cb9d71dbb6281ecdfe84c
scoot556 <scottdjuric@hotmail.com>
2023-10-05 17:26:25 +1100

    added collections route and get movie collection

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,49 @@
 FROM mongo:3
+FROM golang:latest
 
 RUN apt-get update && apt-get install -y golang
 
-RUN go get github.com/gin-gonic/gin
-RUN go get gopkg.in/mgo.v2
-RUN go get gopkg.in/mgo.v2/bson
-RUN go get github.com/jwuensche/trommel
+RUN apt-get install -y git
+
+ENV GOPATH=/go
+
+ENV GOPROXY=https://proxy.golang.org,direct
+
+
+#RUN go get -u github.com/gin-gonic/gin
+#RUN go install gopkg.in/mgo.v3
+#RUN go install gopkg.in/mgo.v3/bson
+#RUN go install github.com/jwuensche/trommel
 
 RUN mkdir -p /go/src/app
 WORKDIR /go/src/app
 
 COPY . .
+RUN go mod download
+RUN go install
+
+RUN apt-get update && \
+    apt-get install -y gnupg && \
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+
+RUN echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/3.6 main" | tee /etc/apt/sources.list.d/mongodb-org-3.6.list && \
+    apt-get update && \
+    apt-get install -y mongodb-org
+
+RUN mkdir -p /data/db
+
+COPY movies.json /movies.json
+COPY comments.json /comments.json
 
 RUN go build -o main
 
-EXPOSE 8080
+EXPOSE 6000
 
-CMD ["./main"]
+CMD mongosh --fork --logpath /var/log/mongodb.log && \
+    mongo --host localhost --eval "db = db.getSiblingDB('ecal'); db.createCollection('movies'); db.movies.insertMany([]);" && \
+    mongo --host localhost --eval "db = db.getSiblingDB('ecal'); db.createCollection('comments'); db.comments.insertMany([]);" && \
+    mongoimport --host localhost --db reach-engine --collection movies --type json --file /movies.json --jsonArray && \
+    mongoimport --host localhost --db reach-engine --collection comments --type json --file /comments.json --jsonArray && \
+    ./main
+
+#CMD ["./main"]
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -13,16 +14,77 @@ import (
 )
 
 func main() {
-	//router := gin.Default()
-	//
-	//router.GET("/", func(c *gin.Context) {
-	//	c.JSON(200, gin.H{
-	//		"data": "Hello from Gin-gonic & mongoDB",
-	//	})
-	//})
+	router := gin.Default()
 
-	err2 := godotenv.Load()
-	if err2 != nil {
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"data": "Hello from Gin-gonic & mongoDB",
+		})
+	})
+	//collections := []string{"movies", "comments"}
+
+	router.GET("/collections", func(c *gin.Context) {
+		collections := GetCollections()
+		c.JSON(200, gin.H{
+			"data": collections,
+		})
+	})
+
+	router.GET("/collections/movies", func(c *gin.Context) {
+		movieCollection, err := GetCollection("movies")
+		if err != nil {
+			log.Fatal(err)
+		}
+		//jsonData, err := json.Marshal(movieCollection)
+		//if err != nil {
+		//	log.Fatal(err)
+		//}
+		c.Header("Content-Type", "application/json")
+		//c.JSON(200, gin.H{
+		//	"data": jsonData,
+		//})
+		c.JSON(200, gin.H{
+			"data": movieCollection,
+		})
+	})
+
+	router.Run("localhost:6000")
+}
+
+func GetCollections() []string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	//
+	mongoURI := os.Getenv("MONGOURI")
+	//
+	clientOptions := options.Client().ApplyURI(mongoURI)
+	//
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//
+	err = client.Ping(context.Background(), readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := client.Database("ecal")
+
+	checkCollections, err := db.ListCollectionNames(context.Background(), options.ListCollections())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(checkCollections)
+	return checkCollections
+}
+
+func GetCollection(collectionName string) ([]bson.M, error) {
+	err := godotenv.Load()
+	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
@@ -30,42 +92,43 @@ func main() {
 
 	clientOptions := options.Client().ApplyURI(mongoURI)
 
-	client, err2 := mongo.Connect(context.Background(), clientOptions)
-	if err2 != nil {
-		log.Fatal(err2)
-	}
-
-	err2 = client.Ping(context.Background(), readpref.Primary())
-	if err2 != nil {
-		log.Fatal(err2)
-	}
-
-	db := client.Database("ecal")
-
-	cursor, err2 := db.ListCollections(context.Background(), bson.D{})
-	if err2 != nil {
-		log.Fatal(err2)
-	}
-	defer cursor.Close(context.Background())
-
-	for cursor.Next(context.Background()) {
-		var collInfo bson.M
-		if err := cursor.Decode(&collInfo); err != nil {
-			log.Fatal(err)
-		}
-
-		// Extract the collection name
-		collectionName := collInfo["name"].(string)
-
-		fmt.Println("Collection Name:", collectionName)
-	}
-
-	if err := cursor.Err(); err != nil {
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	//err := router.Run()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	err = client.Ping(context.Background(), readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := bson.M{}
+
+	db := client.Database("ecal")
+
+	findOptions := options.Find().SetLimit(100)
+
+	getCollection, err := db.Collection(collectionName).Find(context.Background(), filter, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer getCollection.Close(context.Background())
+
+	var results []bson.M
+
+	for getCollection.Next(context.Background()) {
+		var doc bson.M
+		if err := getCollection.Decode(&doc); err != nil {
+			return nil, err
+		}
+
+		results = append(results, doc)
+	}
+
+	if err := getCollection.Err(); err != nil {
+		return nil, err
+	}
+
+	return results, nil
 }
